3e76beb73483f5c1d7fa6560c33401a59160375f
Xuan liu <xuanliu@uw.edu>
2021-12-05 00:35:02 -0800

    adding header

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,17 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <nav class='navbar is-transparent is-fixed-top' role='navigation' aria-label='main navigation'>
+
+      <div className='navbar-start'>
+        <h4 className='navbar-item'><Link to="/">{siteTitle}</Link></h4>
+      </div>
+      <div className='navbar-end'>
+        <h4 className='navbar-item'><Link to="/">Work</Link></h4>
+        <h4 className='navbar-item'><Link to="/about">About</Link></h4>
+      </div>
+
+  </nav>
 )
 
 Header.propTypes = {
