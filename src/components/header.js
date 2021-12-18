72188811fb90f30c00830b9dd61a0c1fbe0d13c7
Xuan liu <xuanliu@uw.edu>
2021-12-18 15:24:47 -0800

    updating nav styling

--- src/components/header.js
+++ src/components/header.js
@@ -5,11 +5,13 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <div className='container is-fluid'>
   <nav class='navbar is-transparent' role='navigation' aria-label='main navigation'>
+      <div className='navbar-menu is-active'>
       <div className='navbar-start'>
         <a className='navbar-item'><Link to='/' target='_blank'>{siteTitle}</Link></a>
       </div>
       <div className='navbar-end'>
         <a className='navbar-item' href='https://drive.google.com/file/d/1eTQO5ls8TMGJsOGg3l1oIxATCj1_HU89/view?usp=sharing' target='_blank'>Resume</a>
+      </div>
       </div>
 
   </nav>
