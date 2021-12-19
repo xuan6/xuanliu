49de5bf0de476a706e98ed5f3745c52905a02a8d
Xuan liu <xuanliu@uw.edu>
2021-12-19 00:23:09 -0800

    adding icons and links to footer;

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,24 @@
-import * as React from "react"
+import * as React from 'react'
+import {FaLinkedin} from '@react-icons/all-files/fa/FaLinkedin'
+import {FaGithubSquare} from '@react-icons/all-files/fa/FaGithubSquare'
+import {MdEmail} from '@react-icons/all-files/md/MdEmail'
 
 const Footer = () => (
     <footer className='container'>
-    © {new Date().getFullYear()} Design and Code by Xuan Liu. 
-    Built with React & 
-    {` `}
-    <a href="https://www.gatsbyjs.com">Gatsby</a>.
+        <div className='footer-text'>© {new Date().getFullYear()} Design and Code by Xuan Liu. 
+        Built with React & Gatsby.
+        </div>
+        <div className='footer-icon'>
+            <a href='https://www.linkedin.com/in/xuanliux/' target='_blank'>
+                <FaLinkedin size='1.5rem'/>
+            </a>
+            <a href='https://github.com/xuan6' target='_blank'>
+                <FaGithubSquare size='1.5rem'/>
+            </a>
+            <a href='mailto:xuanliux93@gmail.com'>
+                <MdEmail size='1.5rem'/>
+            </a>
+        </div>
   </footer>
 )
 
