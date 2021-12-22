bf56fce0fff42e77d35ec5a80231c8ae299bf191
Xuan liu <xuanliu@uw.edu>
2021-12-21 22:02:06 -0800

    fixing styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,8 +15,8 @@ const NotFoundPage = () => (
     <p>While typical designers spend days making creative crafts, Xuan reviews design from the technical lens, systematize designs with engineering patterns, and build scalable, component-based design systems to facilitate design and development process.</p>
     <p>Xuan brings design to life with proof of technical feasibility, scalability, accessibility, and localization compatibility. Xuan is also experienced in exploring short-term alternatives and long-term strategies to meet design intents under technical constraints and tight timelines.</p>
     <p>Working closely with product and engineering teams makes Xuan a efficient collaborator and communicator. Xuan guides product teams to apply reusable patterns and create products with consistency. Xuan also partners with engineering teams through documentation, office hours, and code reviews to drive alignment and front-end fit and finish.</p>
-    <p>Still wants to know more? Please check Xuan’s <a href='https://drive.google.com/file/d/1eTQO5ls8TMGJsOGg3l1oIxATCj1_HU89/view'>resume</a> for details or <a href='mailto:xuanliux93@gmail.com'>drop a line</a>. </p>
-    <p>Thanks for stopping by! :D</p>
+    <p>Still wants to know more? Check Xuan’s <a href='https://drive.google.com/file/d/1eTQO5ls8TMGJsOGg3l1oIxATCj1_HU89/view'>resume</a> for details or <a href='mailto:xuanliux93@gmail.com'>drop a line</a>. </p>
+    <p>Thanks for stopping by.</p>
     </div>
   </div>
   </Layout>
