3101bb565bb72fe0b1f575707deb5e173059f05a
Xuan liu <xuanliu@uw.edu>
2021-12-21 23:11:15 -0800

    updating content

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,10 +12,10 @@ const NotFoundPage = () => (
     <h3>Xuan Liu (she/her) </h3>
     <h4>Design Engineering | Amazon Device Design</h4>
     <p>People often call Xuan Designer by title, but Xuan focuses more on the Engineering side in a nutshell. If you are looking for a typical UX/UI Designer who is passionate about drawing wireframes and visually appealing mockups, you might want to back up and make a u-turn</p>
-    <p>While typical designers spend days making creative crafts, Xuan reviews design from the technical lens, systematize designs with engineering patterns, and build scalable, component-based design systems to facilitate design and development process.</p>
+    <p>While typical designers spend days making creative crafts, Xuan reviews design from the technical lens, systematizes designs with engineering patterns, and builds scalable, component-based design systems to facilitate design and development process.</p>
     <p>Xuan brings design to life with proof of technical feasibility, scalability, accessibility, and localization compatibility. Xuan is also experienced in exploring short-term alternatives and long-term strategies to meet design intents under technical constraints and tight timelines.</p>
-    <p>Working closely with product and engineering teams makes Xuan a efficient collaborator and communicator. Xuan guides product teams to apply reusable patterns and create products with consistency. Xuan also partners with engineering teams through documentation, office hours, and code reviews to drive alignment and front-end fit and finish.</p>
-    <p>Still wants to know more? Check Xuan’s <a href='https://drive.google.com/file/d/1eTQO5ls8TMGJsOGg3l1oIxATCj1_HU89/view'>resume</a> for details or <a href='mailto:xuanliux93@gmail.com'>drop a line</a>. </p>
+    <p>Working closely with product and engineering teams makes Xuan a efficient collaborator and communicator. Xuan guides product teams to identify and apply reusable patterns and create products with consistency. Xuan also partners with engineering teams through documentation, office hours, and code reviews to ensure the parity between design and development.</p>
+    <p>You are still there and want to know more? Feel free to check Xuan’s <a href='https://drive.google.com/file/d/1eTQO5ls8TMGJsOGg3l1oIxATCj1_HU89/view'>resume</a> or <a href='mailto:xuanliux93@gmail.com'>drop a line</a>.</p>
     <p>Thanks for stopping by.</p>
     </div>
   </div>
