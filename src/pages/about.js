6bfe32b2eeeeb51cdd11b25491aa0fd8f943a4cd
Xuan liu <xuanliu@uw.edu>
2022-01-08 11:34:46 -0800

    updating info

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import './styles.css'
 
-const NotFoundPage = () => (
+const About = () => (
   <Layout>
   <Seo title="About" />
   <div className='container'>
@@ -13,7 +13,7 @@ const NotFoundPage = () => (
     <h4>Design Engineering | Amazon Device Design</h4>
     <p>xuanliux93 [at] gmail [dot] com<br/> <a href='https://drive.google.com/file/d/1eTQO5ls8TMGJsOGg3l1oIxATCj1_HU89/view'>resume</a></p>
     <p>People often call Xuan Designer by title, but Xuan focuses more on the Engineering side in a nutshell.</p>
-    <p>While typical Product Designers spend days making creative crafts, Xuan, as a System Designer, reviews design from the technical lens, systematizes designs with engineering patterns, and builds scalable, component-based design systems to facilitate design and development process.</p>
+    <p>While typical Product Designers spend days making visually appealing crafts, Xuan, as a System Designer, reviews design from the technical lens, systematizes designs with engineering patterns, and builds scalable, component-based design systems to facilitate design and development process.</p>
     <p>Xuan brings design to life with proof of technical feasibility, scalability, accessibility, and localization compatibility. Xuan is also experienced in exploring short-term alternatives and long-term strategies to meet design intents under technical constraints and tight timelines.</p>
     <p>Working closely with product and engineering teams makes Xuan an efficient collaborator and communicator. Xuan guides product teams to identify and apply reusable patterns and create products with consistency. Xuan also partners with engineering teams through documentation, office hours, and code reviews to ensure the parity between design and development.</p>
     
@@ -22,4 +22,4 @@ const NotFoundPage = () => (
   </Layout>
 )
 
-export default NotFoundPage
+export default About
